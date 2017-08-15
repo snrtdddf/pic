e3dc630ce9b191f2aa74c9d1e863f790d528d1c1
snrtdddf <snrtdddf@126.com>
2017-08-15 08:44:16 +0800

    修改关键字找图接口

--- src/main/java/com/k4meitu/pic/controller/group/FindKeywordController.java
+++ src/main/java/com/k4meitu/pic/controller/group/FindKeywordController.java
@@ -47,6 +47,21 @@ public class FindKeywordController{
 						for(int k=0; k<list.size(); k++){
 							PicGroupModel model = list.get(k);
 							model.setImgUrl(ApiConstant.ImgPath + model.getGroupId()+"/"+model.getImgCoverName());
+							if (model.getType().equals("xinggan")) {
+								model.setType("性感");
+							}else if (model.getType().equals("qingchun")) {
+								model.setType("清纯");
+							}else if (model.getType().equals("chemo")) {
+								model.setType("车模");
+							}else if (model.getType().equals("xiaohua")) {
+								model.setType("校花");
+							}else if (model.getType().equals("qipao")) {
+								model.setType("旗袍");
+							}else if (model.getType().equals("mmjpg_home")) {
+								model.setType("美女");
+							}else if (model.getType().equals("mingxing")) {
+								model.setType("明星");
+							}
 						}
 					}
 					map.put("maxPage", totalPage/pCount);
